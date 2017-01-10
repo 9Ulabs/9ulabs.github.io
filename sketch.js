4747f236241b21a642293aa506d07cfb300ef36b
Shailendra Paliwal <hello@shailendra.me>
2017-01-10 15:08:58 +0530

    grayscaled

--- sketch.js
+++ sketch.js
@@ -10,19 +10,19 @@ function setup() {
 }
 
 function draw() {
-  background(sin(frameCount/40)*210, cos(frameCount/100)*100, sin(frameCount/50 + 50)*origin);
-  fill(sin(frameCount/45 + 15)*150 + 100, 34, sin(frameCount/500)*255 + 55);
-  strokeWeight(25);
+  background(sin(frameCount/300)*150+50);
+  fill(sin(frameCount/45 + 15)*150 + 100*random());
+  strokeWeight((frameCount%20)*random());
   ellipse(50, 50, 160, 160);
-  stroke(50, 134, cos(frameCount/187)*100);
-  rect(140, 140, 135, 245);
-  fill(123, 64, 238);
-  strokeWeight(5);
-  stroke(17, 117, 217);
-  quad(sin(frameCount/120)*350 + 100, 350, mouseX, 57, 187, mouseY, 89, 999, 599);
+  stroke(cos(frameCount/187)*100);
+  rect(160, 130, 135, 245);
+  fill(123);
+  strokeWeight(random());
+  stroke(17);
+  quad(sin(frameCount/120)*500 + 100, 350, mouseX, 57, 300, mouseY, 89, 500, 599);
  
   for (var i = 0; i < 12; i++) {
-    fill(100+(mouseX%100), 255-(fillstep*i), 200);
+    fill(255-(fillstep*i)*random());
     ellipse(xpos+(xstep*i), ypos, 20, 20+(hstep*i));
   }
   
@@ -45,12 +45,13 @@ function draw() {
       40);
   }
   
-  text("Here, millions of mixed shades and shadows,", 870, 230);
-  text("drowned dreams, somnambulisms, reveries;", 870, 245);
-  text("all that we call lives and souls,", 870, 260);
-  text("lie dreaming, dreaming, still;", 870, 275);
-  text("tossing like slumberers in their beds;", 870, 290);
-  text("the ever-rolling waves", 870, 305);
-  text("but made so by their restlessness.", 870, 320);
-  text("Herman Melville", 870, 380);
+  fill(cos(frameCount/300)*150+50);
+  textSize(16);
+  textFont("Helvetica");
+  textStyle(ITALIC);
+  text("Technology is not bringing things", 860, 260);
+  text("near but it is expanding the here", 860, 280);
+  text("into an infinite space.", 860, 300);
+  textStyle(NORMAL);
+  text("Tom McCarthy", 860, 355);
 }
